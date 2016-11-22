b1f020f7eca5985daa83ec3bc712ad457bdf18f1
p455w0rd <p455w0rd@gmail.com>
2016-11-22 01:34:23 -0500

    added flame particle to cendelabra/chandelier

--- src/main/java/wiiv/emporium/block/BlockLampCandelabra.java
+++ src/main/java/wiiv/emporium/block/BlockLampCandelabra.java
@@ -1,9 +1,14 @@
 package wiiv.emporium.block;
 
+import java.util.Random;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.*;
 
 public class BlockLampCandelabra extends BlockBaseColorable16 {
 
@@ -25,8 +30,17 @@ public class BlockLampCandelabra extends BlockBaseColorable16 {
 
 	@Override
 	public BlockRenderLayer getBlockLayer() {
-
 		return BlockRenderLayer.TRANSLUCENT;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.1D, pos.getY() + 0.8D, pos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D, new int[0]);
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.5D, pos.getY() + 0.8D, pos.getZ() + 0.1D, 0.0D, 0.0D, 0.0D, new int[0]);
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.9D, pos.getY() + 0.8D, pos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D, new int[0]);
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.5D, pos.getY() + 0.8D, pos.getZ() + 0.9D, 0.0D, 0.0D, 0.0D, new int[0]);
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.5D, pos.getY() + 0.9D, pos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D, new int[0]);
 	}
 
 }
--- src/main/java/wiiv/emporium/block/BlockLampChandelier.java
+++ src/main/java/wiiv/emporium/block/BlockLampChandelier.java
@@ -1,9 +1,14 @@
 package wiiv.emporium.block;
 
+import java.util.Random;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.*;
 
 public class BlockLampChandelier extends BlockBaseColorable16 {
 
@@ -27,6 +32,15 @@ public class BlockLampChandelier extends BlockBaseColorable16 {
 	public BlockRenderLayer getBlockLayer() {
 
 		return BlockRenderLayer.TRANSLUCENT;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.1D, pos.getY() + 0.8D, pos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D, new int[0]);
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.5D, pos.getY() + 0.8D, pos.getZ() + 0.1D, 0.0D, 0.0D, 0.0D, new int[0]);
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.9D, pos.getY() + 0.8D, pos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D, new int[0]);
+		worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + 0.5D, pos.getY() + 0.8D, pos.getZ() + 0.9D, 0.0D, 0.0D, 0.0D, new int[0]);
 	}
 
 }
